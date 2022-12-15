c7370313408b274394c9399e8b641f1298f8b1fe
Taren Sanders <taren.sanders@acu.edu.au>
2022-12-15 21:27:25 +1100

    Add helper to get distinct (#3)

---
+++ R/utils.R
@@ -0,0 +1,17 @@
+create_distinct <- function(df) {
+  variables <- c(
+    "SES", "Ethnicity", "Sleep conditions", "Maturational status",
+    "Sleep medications"
+  )
+
+  dir.create("temp", showWarnings = FALSE)
+
+  purrr::map(
+    variables,
+    ~ df %>%
+      dplyr::mutate(studyid = gsub(".*(\\d{3}).*", "\\1", studyid)) %>%
+      dplyr::distinct(dplyr::across(dplyr::all_of(c("studyid", .x)))) %>%
+      dplyr::filter(!is.na(dplyr::across(dplyr::all_of(.x)))) %>%
+      readr::write_csv(file.path("temp", paste0(.x, ".csv", collapse = "")))
+  )
+}
