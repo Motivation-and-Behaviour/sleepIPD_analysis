ba74537c361989345dc60b77d272fb810c15a579
conig <james.conigrave@gmail.com>
2023-01-23 15:32:26 +1100

    init mice

--- R/make_data_imp.R
+++ R/make_data_imp.R
@@ -3,12 +3,21 @@
 #' .. content for \details{} ..
 #'
 #' @title
-#' @param data_imp
+#' @param data data holdout or clean data
+#' @param n_imps number of imputations
 #' @return
 #' @author conig
 #' @export
-make_data_imp <- function(data_imp) {
+
+make_data_imp <- function(data, n_imps = 3) {
 
   NULL
-
+  # data <- janitor::clean_names(data)
+  # data <- data[sample(seq_len(nrow(data)), 2000),]
+  #
+  #
+  # require(mice)
+  # imps <- mice(data, remove.collinear=FALSE)
+  #
+  # imps
 }
