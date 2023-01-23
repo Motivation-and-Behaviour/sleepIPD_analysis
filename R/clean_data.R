de0f0c8c17881f15b934ec20cbbb4ba16c06dc20
Taren Sanders <Taren.Sanders@acu.edu.au>
2023-01-23 13:57:32 +1100

    First pass of clean_data to output six main variables
    Fixes #8

--- R/clean_data.R
+++ R/clean_data.R
@@ -8,5 +8,26 @@
 #' @author
 #' @export
 clean_data <- function(data_joined) {
-  NULL
+  data_joined %>%
+    # Remove problem studies
+    # TODO: Remove when data are fixed
+    filter(!studyid %in%
+      c(
+        "data/100_ISCOLE.csv", "data/102_Camilla.csv", "data/115_Angelica.csv"
+      )) %>%
+    # Rename data
+    rename(
+      pa_volume = ACC_day_mg,
+      pa_intensity = ig_gradient,
+      sleep_duration = dur_spt_sleep_min, # TODO: confirm
+      sleep_efficiency = sleep_efficiency,
+      sleep_onset = sleeponset_p5,
+      sleep_wakeup = wakeup_p5,
+      sleep_onset_time = sleeponset_ts_p5,
+      sleep_wakeup_time = wakeup_ts_p5,
+      sleep_regularity = SleepRegularityIndex
+    ) %>%
+    # Filter for OK data
+    # TODO: Decision rules for this
+    filter((is.na(sleep_duration) | sleep_duration > 300))
 }
