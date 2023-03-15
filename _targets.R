6c75cf703882b50f87f2dbc01044784612fd28cf
Taren Sanders <taren.sanders@acu.edu.au>
2023-03-15 14:48:30 +1100

    Restructure _targets file

--- _targets.R
+++ _targets.R
@@ -10,25 +10,20 @@ load_packages()
 list(
   tar_files_input(datasets, list.files("data", full.names = TRUE)),
   tar_target(data_raw,
-             read_csv(datasets,
-                      col_types = list(.default = "c"),
-                      id = "studyid"
-             ),
-             pattern = map(datasets), iteration = "list"
+    read_csv(datasets,
+      col_types = list(.default = "c"),
+      id = "studyid"
+    ),
+    pattern = map(datasets), iteration = "list"
   ),
 
   # Data targets
   tar_target(data_joined, bind_rows(data_raw), pattern = map(data_raw)),
   tar_target(data_clean, clean_data(data_joined)),
   tar_target(data_holdout, make_data_holdout(data_clean)),
-
-  tar_target(table_1, make_table1(data_clean)),
-  tar_target(region_lookup, make_region_lookup()),
-  tar_target(demog_table, make_demog_table(table_1, region_lookup)),
   tar_target(data_imp, make_data_imp(data_holdout, n_imps = 3)),
 
   # Modelling targets
-
   tar_target(model_list, make_model_list(data_imp)),
   tar_target(model_diagnostics, make_model_diagnostics(model_list)),
 
@@ -36,7 +31,14 @@ list(
   tar_target(explore_img, make_explore_img_list(data_holdout)),
   tar_target(purdy_pictures, produce_purdy_pictures(model_list)),
 
+  # Tables
+  tar_target(table_1, make_table1(data_clean)),
+  tar_target(region_lookup, make_region_lookup()),
+  tar_target(demog_table, make_demog_table(table_1, region_lookup)),
+
   # Output manuscript
-  tar_render(manuscript, "doc/manuscript.Rmd", output_format = c("papaja::apa6_docx",
-                                                                 "papaja::apa6_pdf"))
+  tar_render(manuscript, "doc/manuscript.Rmd", output_format = c(
+    "papaja::apa6_docx",
+    "papaja::apa6_pdf"
+  ))
 )
