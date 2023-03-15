69f1a4449b6c4ae3a357b38f4ba852196042b21a
Taren Sanders <taren.sanders@gmail.com>
2023-03-15 22:27:53 +1100

    Add PA tables and tidy Rmd

--- R/make_model_tables.R
+++ R/make_model_tables.R
@@ -16,13 +16,23 @@ make_model_tables <- function(model_list) {
     `Sleep onset` = "scale_sleep_onset",
     `Sleep regularity` = "scale_sleep_regularity"
   )
-  pa_vars <- c("scale_pa_volume", "scale_pa_intensity")
+
+  pa_vars <- list(
+    `Physical activity volume` = "scale_pa_volume",
+    `Physical activity intensity` = "scale_pa_intensity"
+  )
 
   sleep_table <- list()
 
   sleep_table$data <- lapply(sleep_vars, function(sleep) {
-    model1_name <- paste(sleep, "by", pa_vars[[1]], collapse = " ")
-    model2_name <- paste(sleep, "by", pa_vars[[2]], collapse = " ")
+    model1_name <- paste(
+      sleep, "by", pa_vars["Physical activity volume"],
+      collapse = " "
+    )
+    model2_name <- paste(
+      sleep, "by", pa_vars[["Physical activity intensity"]],
+      collapse = " "
+    )
 
     tab <- cbind(
       format_table(model_list[[model1_name]]$table),
@@ -32,14 +42,40 @@ make_model_tables <- function(model_list) {
   })
 
   sleep_table$caption <-
-    "Sleep on physical activity volume controlling for SES, gender and BMI"
+    "Physical activity on sleep controlling for SES, gender and BMI"
   sleep_table$note <- note
   sleep_table$col_spanners <- list(
     "Physical Activity Volume" = c(2, 5),
     "Physical Activity Intensity" = c(6, 9)
   )
 
-  return(list(sleep = sleep_table))
+  pa_table <- list()
+  pa_table$data <- lapply(sleep_vars, function(sleep) {
+    model1_name <- paste(
+      pa_vars["Physical activity volume"], "by", paste0(sleep, "_lag"),
+      collapse = " "
+    )
+    model2_name <- paste(
+      pa_vars[["Physical activity intensity"]], "by", paste0(sleep, "_lag"),
+      collapse = " "
+    )
+
+    tab <- cbind(
+      format_table(model_list[[model1_name]]$table),
+      format_table(model_list[[model2_name]]$table)
+    )
+    tab[, c(1:5, 7:10)]
+  })
+
+  pa_table$caption <-
+    "Sleep on physical activity controlling for SES, gender and BMI"
+  pa_table$note <- note
+  pa_table$col_spanners <- list(
+    "Physical Activity Volume" = c(2, 5),
+    "Physical Activity Intensity" = c(6, 9)
+  )
+
+  return(list(sleep = sleep_table, physical_activity = pa_table))
 }
 
 format_table <- function(tab) {
@@ -51,8 +87,13 @@ format_table <- function(tab) {
   tab <- tab[!grepl("^bmi", tab$term), ]
   tab$term <- stringr::str_to_sentence(tab$term)
   tab$term <- gsub(
-    "(S|s)cale pa (intensity|volume)", "Physical Activity", tab$term
+    "(S|s)cale pa (intensity|volume)", "Physical activity", tab$term
   )
+  tab$term <- gsub(
+    "(S|s)cale sleep", "Sleep", tab$term
+  )
+  tab$term <- gsub("\\slag", "", tab$term)
+  tab$term <- gsub(":", " $\\\\times$ ", tab$term)
   names(tab) <- c("Term", "$\\beta$ [95\\% CI]", "SE", "t", "p")
   tab
 }
