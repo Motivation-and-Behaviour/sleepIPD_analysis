ccf8036130a52b303aee5d9f0653b9e8f2444bd7
conig <james.conigrave@gmail.com>
2023-03-24 16:08:20 +1100

    2l imp for obs variables. Simplify sex imputation to get rid of convergence warnings.

--- R/make_data_imp.R
+++ R/make_data_imp.R
@@ -11,34 +11,60 @@
 #' @export
 
 make_data_imp <- function(data, n_imps = 3) {
-  data <- data %>%
+  imp_data <- data %>%
     select(-n_valid_hours, -n_hours, -weekday_x, -day_zero) |>
     dplyr::mutate(participant_id = as.integer(factor(participant_id)))
 
   # Empty imputation to change defaults:
-  m0 <- mice(data, maxit = 0)
+  m0 <- mice(imp_data, maxit = 0)
 
   # Don't do imputation based on these vars:
-  dont_imp <- c("age_cat", "filename", "calendar_date")
+  dont_imp <- c("filename", "calendar_date")
+  dont_use <- c("age_cat", "studyid", "participant_id", "country" ,
+                "accelerometer_wear_location", "accelerometer_model")
+  # Don't imp some vars, and disable some as predictors
   meth <- m0$method
   pred <- m0$predictorMatrix
   meth[names(meth) %in% dont_imp] <- ""
-  pred[, colnames(pred) %in% dont_imp] <- 0
+  pred[, colnames(pred) %in% c(dont_use, dont_imp)] <- 0
+
+  participant_invar <- c("age","weight","height","bmi")
+
+  participant_cont <-
+    c("pa_volume",
+      "pa_intensity",
+      "sleep_duration",
+      "sleep_efficiency",
+      "sleep_onset",
+      "sleep_wakeup",
+      "sleep_onset_time",
+      "sleep_wakeup_time",
+      "sleep_regularity",
+      "sleep_efficiency_lag",
+      "sleep_onset_lag",
+      "sleep_wakeup_lag",
+      "sleep_onset_time_lag",
+      "sleep_wakeup_time_lag",
+      "sleep_regularity_lag",
+      "sleep_duration_lag"
+    )
 
   # Multi-level imputation, consider correlations within participant
-  pred[c("age", "weight", "bmi", "sex"), "participant_id"] <- -2L
-  meth[c("age","weight", "bmi")] <- "2lonly.pmm"
-  meth["sex"] <- "2l.bin"
+  pred["sex",] <- 0
+  pred["sex", c("age","bmi","pa_intensity","screen_time","sleep_regularity")] <- 1
+  pred[c(participant_cont, participant_invar, "sex"), "participant_id"] <- -2L
+  meth[c(participant_cont)] <- "2l.norm"
+  meth[c(participant_invar)] <- "2lonly.pmm"
+  meth["sex"] <- "2lonly.pmm"
 
   # Run imps with better settings
   future_cores <- min(parallel::detectCores() - 1, n_imps)
 
   imps <-
     futuremice(
-      data,
+      imp_data,
       m = n_imps,
       predictorMatrix = pred,
-      cluster_var = list("age" = "participant_id", "weight" = "participant_id", "bmi" = "participant_id", "sex" = "participant_id"),
       method = meth,
       n.core = future_cores
     )
@@ -50,7 +76,6 @@ make_data_imp <- function(data, n_imps = 3) {
     "sleep_onset",
     "sleep_regularity"
   )
-
 
   variables_to_scale <-
     c(
