08f82d7506d4fae0d2882454495d0c9dc219fe58
James Conigrave <12742211+conig@users.noreply.github.com>
2023-03-30 15:48:25 +1100

    Reduce size of model_lists

--- R/make_model_diagnostics.R
+++ R/make_model_diagnostics.R
@@ -7,55 +7,64 @@
 #' @author conig
 #' @export
 make_model_diagnostics <- function(...) {
-
   lst <- list(...)
   names_lst <- as.character(substitute(list(...)))[-1]
+
+  lst <- lapply(lst, check_list)
+
   names(lst) <- names_lst |>
     gsub("model_list_by", "models moderated by", x = _ ) |>
-    gsub("_", " ", x = _ ) |>
-    stringr::str_to_sentence()
+    stringr::str_to_sentence() |>
+    gsub("scale_", "", x = _ ) |>
+    gsub("_lag", "(lagged)", x = _ ) |>
+    gsub("pa|Pa", "PA", x = _ ) |>
+    gsub("_", " ", x = _ )
 
-  lapply(lst, check_model)
+  lst
+}
+
+check_list <- function(model_list){
+
+  info <- lapply(model_list, function(x){
+    x$model_assets$diagnostics
+  }) |> data.table::rbindlist()
+
+  info$model_name <- names(model_list)|>
+    stringr::str_to_sentence() |>
+    gsub("scale_", "", x = _ ) |>
+    gsub("_lag", "(lagged)", x = _ ) |>
+    gsub("pa|Pa", "PA", x = _ ) |>
+    gsub("_", " ", x = _ )
+
+  info[, .("Model name" = model_name, Skewness, Kurtosis, `Converged (\\%)`)]
+
 
 }
 
 #' check_model
 #'
 #' Check the assumptions of a single model
+#' @param model list of models
+#' @param conv convergence proportion
 #' @test model <- model_list_by_age
 
-check_model <- function(model){
-
-  out <- seq_len(length(model)) |> lapply(function(x){
-    model_name <- names(model)[x]
-    x <- model[[x]]
-    moderator <- moderator <- attr(x, "moderator")
-    p_conv <- attr(x$pooled_model, "conv")*100
+check_model <- function(model, conv){
+    p_conv <- conv *100
     pc_conv <- papaja::print_num(p_conv) |>
       paste0("%")
 
-    cbind("Model name" = model_name, check_resids(x),
-          "Converged (\\%)" = pc_conv)
-
-  }) |> data.table::rbindlist()
-
-  out$"Model name" = gsub("scale_", "", out$"Model name") |>
-    gsub("pa|Pa", "PA", x = _ ) |>
-    gsub("_lag", "(lagged)", x = _ ) |>
-    gsub("_", " ", x = _ )
-
-
-  out
-
+    cbind(check_resids(model),
+          "Converged (\\%)" = pc_conv) |> data.table::data.table()
 }
 
 #' check_resids
 #'
 #' Get skewness and kurtosis for models
+#' @param model list of models
 
 check_resids <- function(model){
   require(dplyr)
-  dt <- model$model |> lapply(function(x){
+  dt <- model |> lapply(function(x){
     resid <- residuals(x)
     tibble(Skewness = moments::skewness(resid),
            Kurtosis = 3 - moments::kurtosis(resid))
--- R/model_builder.R
+++ R/model_builder.R
@@ -59,19 +59,26 @@ fit_model <- function(..., data, max_iter = 1e6) {
 #' @param data_imp mids object
 #' @param outcome chatacter. outcome variable name
 #' @param predictors a chatacter vector of predictors
+#' @param moderator a character. Variable name of moderator
+#' @param control_vars a vector of control variables
 #' @param table_only if TRUE, only the table will be retured
 #' @param ranef random effects to paste to formula
+#' @param terms character string of terms to pass to ggeffects
+#' @param RQ numeric to pass to get_effects
 #' @protocol to examine the relationship between sleep and physical activity (Research Questions 1-2) we will use study fixed-effects to account for the nesting of participants in studies (Curran et al 2009). Fixed-effects (not the same as complete pooling analysis that ignores data nesting) control for all time-invariant between-study variance and will allow us to explore within study associations and moderators. We will nest individuals within days, and days within study. We will examine both main effects and subpopulation effects (using separate models), including the following pre-specified individual-level moderators; age (chronological), body mass index z-score (z transformed), SES, ethnicity, and sex as categorical. Day of the week, season (summer vs winter), geographic location, and daylight length will also be included as moderators because these influence sleep and physical activity. Accelerometer wear location will be included as a moderator. Sleep and physical activity may be temporally related where early morning and late evening physical activity can negatively influence optimum sleep duration and sleep quality. To account for this, we will include the time of the day corresponding to the most active periods of physical activity as a moderator. The most active 60, 30, 15, 10 and 5 minutes within 4 windows of time; midnight to 6am (early), 6am to 12pm (normal), 12pm -6pm (normal), 6pm -midnight (late) will be extracted from GGIR and used to test the effect of physical activity proximity to bedtime and wake time on sleep.
 
-#' @test-arguments outcome = "sleep_duration" predictors = c("pa_volume", "pa_intensity")
+#' @test-arguments outcome = "sleep_duration", predictors = "scale_pa_volume * age + I(scale_pa_volume^2) * age", control_vars = c(), table_only = FALSE, ranef  = "(1|studyid) + (1|participant_id)", terms = c("scale_pa_volume[-4:4 by = 0.1]", "age [11, 18, 35, 65]"), moderator = "age"
 
 model_builder <-
   function(data_imp,
            outcome,
            predictors,
+           moderator,
            control_vars = c(),
            table_only = TRUE,
-           ranef) {
+           ranef,
+           terms,
+           RQ) {
     require(broom.mixed)
     require(lme4)
     require(data.table)
@@ -94,7 +101,6 @@ model_builder <-
     conv_p <- sum(conv) / length(conv)
 
     m_pooled <- mice::pool(m)
-    attr(m_pooled, "conv_p") <- conv_p
     pool_summary <- data.table(summary(m_pooled))
 
     crit.val <- qnorm(1 - 0.05 / 2)
@@ -130,8 +136,20 @@ model_builder <-
       return(tabby)
     }
 
+    # Model_assets
+    model_assets <- list(effects = get_effects(
+      m,
+      moderator = moderator,
+      terms = terms,
+      outcome = outcome,
+      conv = conv_p,
+      RQ = RQ
+    ),
+    conv = conv_p,
+    diagnostics = check_model(m, conv = conv_p))
+
     list(
-      model = m,
+      model_assets = model_assets,
       pooled_model = m_pooled,
       table = tabby,
       note = note,
@@ -142,25 +160,40 @@ model_builder <-
 #' get_effects
 #'
 #' Plot an effects display for a RQ1 model
-#' @param model a list delivered by model_builder_RQ1
-#' @param terms character. variables to plot
-#' @param engine ggeffects function
-#' @param plot if TRUE plot is returned
+#' @param model a list delivered by model_builder
+#' @param moderator character. Moderator name
+#' @param terms character vector. terms for effect predictors
+#' @param outcome character. outcome variable
+#' @param conv convergence as a proportion
+#' @param RQ Research question number
 #' @param ... additional arguments passed to the ggeffect engine
 #' @example model = rq1_example_model, terms = c("pa_intensity", "pa_volume")
 
-get_effects <- function(model, terms, engine = ggeffects::ggpredict, plot = FALSE,
-                        ...) {
-  effects <-
-    lapply(seq_len(length(model$model)), function(i) {
-      engine(model$model[[i]], terms = attr(model, "terms"), ...)
-    }) |>
-    ggeffects::pool_predictions()
+get_effects <- function(model, moderator, terms, outcome, conv, RQ, ...) {
 
-  if (!plot) {
-    return(effects)
+  effects <-
+    lapply(seq_len(length(model)), function(i) {
+      suppressMessages(ggeffects::ggpredict(model[[i]], terms = terms, ...))
+    }) |> ggeffects::pool_predictions()
+
+  conv_print <- paste0(papaja::print_num((1 -conv) * 100), "%")
+
+  dt <- data.table(effects)
+  dt$x_name <- terms[1]
+  dt$group_name <- terms[2]
+  dt$outcome <- outcome
+  dt$RQ <- RQ
+  dt$moderator <- moderator
+  dt$conv_p <- conv
+  if(conv < .75){
+    dt$message <- as.character(glue::glue("DID NOT CONVERGE ({conv_print})"))
+  }else{
+    dt$message <- " "
   }
-  effects |>
-    plot() +
-    figure_theme()
+  attr(dt, "conv") <- conv
+  dt
+
 }
+
+
+
