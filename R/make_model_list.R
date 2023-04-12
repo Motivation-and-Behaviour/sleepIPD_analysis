59be6800b0eadacba5a900c70985b7957a4339d6
Taren Sanders <taren.sanders@gmail.com>
2023-04-12 21:02:28 +1000

    Add vars attribute to model_list
    Co-authored-by: James Conigrave <12742211+conig@users.noreply.github.com>

--- R/make_model_list.R
+++ R/make_model_list.R
@@ -17,15 +17,16 @@ make_model_list <- function(data_imp,
                               "scale_sleep_onset", "scale_sleep_regularity"
                             ),
                             pa_vars = c(
-                              "scale_pa_volume", "scale_pa_intensity"
+                              "log_pa_volume", "scale_pa_intensity"
                             )) {
   control_vars <- control_vars[!control_vars == moderator]
 
   sleep_lag_vars <- paste0(sleep_vars, "_lag")
 
-  make_quadratic <- function(x) glue::glue("{x} * {moderator} + I({x}^2) * {moderator}")
+  quadratic_pattern <- "{x} * {moderator} + I({x}^2) * {moderator}"
+  make_quadratic <- function(x) glue::glue(quadratic_pattern)
 
-  instructions.rq1 <-
+  instructions_rq1 <-
     expand.grid(
       outcome = sleep_vars,
       predictors = make_quadratic(pa_vars),
@@ -33,7 +34,7 @@ make_model_list <- function(data_imp,
       stringsAsFactors = FALSE
     )
 
-  instructions.rq3 <-
+  instructions_rq3 <-
     expand.grid(
       outcome = pa_vars,
       predictors = make_quadratic(sleep_lag_vars),
@@ -41,9 +42,11 @@ make_model_list <- function(data_imp,
       stringsAsFactors = FALSE
     )
 
-  instructions <- rbind(instructions.rq1, instructions.rq3)
+  instructions <- rbind(instructions_rq1, instructions_rq3)
 
-  instructions$model_name <- with(instructions, glue::glue("{outcome} by {gsub(' .*', '', predictors)}"))
+  instructions$model_name <- with(
+    instructions, glue::glue("{outcome} by {gsub(' .*', '', predictors)}")
+  )
 
   out <- lapply(
     seq_len(length(instructions[, 1])),
@@ -57,13 +60,14 @@ make_model_list <- function(data_imp,
         table_only = FALSE,
         ranef = ranef,
         terms = c(
-          paste0(gsub(" .*", "", instructions[i, "predictors"]), "[-4:4 by = 0.1]"),
+          paste0(
+            gsub(" .*", "", instructions[i, "predictors"]), "[-4:4 by = 0.1]"),
           glue::glue("{moderator} [{moderator_term}]")
         ),
         RQ = instructions[i, "RQ"]
       )
 
-      attr(model, "RQ") <- instructions[i, "RQ"]
+      attr(model, "RQ") <- instructions[i, "RQ"] # nolint: object_name_linter.
       attr(model, "moderator") <- moderator
 
       model
@@ -71,5 +75,6 @@ make_model_list <- function(data_imp,
   )
 
   names(out) <- instructions[, "model_name"]
+  attr(out, "vars") <- list(sleep_vars = sleep_vars, pa_vars = pa_vars)
   out
 }
