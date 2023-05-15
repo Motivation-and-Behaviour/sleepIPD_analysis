1730bb9eb5a1b09cad6c89190627306c328ff163
Taren Sanders <taren.sanders@acu.edu.au>
2023-05-15 15:11:18 +1000

    Add helper file to run pipeline

---
+++ make.R
@@ -0,0 +1,12 @@
+# Easier running of the pipeline
+library(targets)
+
+# Start a UI to monitor pipeline
+tar_watch(seconds = 45, targets_only = TRUE, outdated = TRUE, display = "graph")
+
+# Run everything up to imputation checks sequentially
+tar_make(c(imputation_checks, multiverse_file))
+
+# Run everything else in parallel
+# No advantage after 14 cores
+tar_make_future(workers = min(parallel::detectCores(), 14))
