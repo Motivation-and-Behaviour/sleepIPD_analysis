964c786d17b87877f0c4b0736da1333d3b25677b
Taren Sanders <taren.sanders@acu.edu.au>
2024-09-17 14:04:02 +1000

    Add objects from full run of pipeline (#100)
    * Full run of pipeline less pdfs
    
    * Add rendered manuscripts
    
    * Adjust manuscript to match results
    
    * Fix incorrect sentence in results
    
    * Rerun pipeline
    
    * Add Dockerfile

--- make.R
+++ make.R
@@ -1,4 +1,5 @@
 #!/usr/bin/env Rscript
+args <- commandArgs(trailingOnly = TRUE)
 # Check for GCS
 if (Sys.getenv("GCS_AUTH_FILE") == "") {
   usethis::ui_warn(
@@ -12,12 +13,21 @@ if (Sys.getenv("GCS_AUTH_FILE") == "") {
 # Easier running of the pipeline
 library(targets)
 
-# Start a UI to monitor pipeline
-tar_watch(seconds = 45, targets_only = TRUE, outdated = TRUE, display = "graph")
+if ("--watch" %in% args) {
+  # Start a UI to monitor pipeline
+  tar_watch(
+    seconds = 45, targets_only = TRUE, outdated = TRUE, display = "graph"
+  )
+}
 
-# Run everything up to imputation checks sequentially
-tar_make(c(imputation_checks, multiverse_file))
+if ("--parallel" %in% args) {
+  # Run everything up to imputation checks sequentially
+  tar_make(c(imputation_checks, multiverse_file))
 
-# Run everything else in parallel
-# No advantage after 14 cores
-tar_make_future(workers = min(parallel::detectCores(), 14))
+  # Run everything else in parallel
+  # No advantage after 14 cores
+  tar_make_future(-manuscript, workers = min(parallel::detectCores(), 8))
+} else {
+  # Run everything sequentially
+  tar_make()
+}
