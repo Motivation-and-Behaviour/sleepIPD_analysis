1eab534bfcf09ed99ca847e6237c54b1c1d90826
Taren Sanders <taren.sanders@acu.edu.au>
2024-09-17 05:23:19 +0000

    Add helper to build manuscript only

--- make.R
+++ make.R
@@ -20,6 +20,12 @@ if ("--watch" %in% args) {
   )
 }
 
+if ("--manuscript-only" %in% args) {
+  # Only update manuscript and direct dependencies
+  targets::tar_make(c(references, manuscript), shortcut = TRUE)
+  quit(status = 0)
+}
+
 if ("--parallel" %in% args) {
   # Run everything up to imputation checks sequentially
   tar_make(c(imputation_checks, multiverse_file))
@@ -27,6 +33,8 @@ if ("--parallel" %in% args) {
   # Run everything else in parallel
   # No advantage after 14 cores
   tar_make_future(-manuscript, workers = min(parallel::detectCores(), 8))
+
+  targets::tar_make(manuscript, shortcut = TRUE)
 } else {
   # Run everything sequentially
   tar_make()
